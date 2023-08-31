640fca92930578a2653190920f902d2d5fe5b245
mateusvillarroel <142513404+mateusvillarroel@users.noreply.github.com>
2023-08-31 11:53:08 -0300

    Add files via upload

---
+++ listasDict.py
@@ -0,0 +1,15 @@
+listaInterna = ["lista", 1]
+compras = ["andre", 1234, 1.2, listaInterna, 112]
+print (compras[3])
+compras[3] = 51
+print (compras[0 : len(compras) : 3])
+lista = []
+tupla = ()
+dicionario = {}
+dicionario ["Professores"] = ["Andre", "denis"]
+dicionario ["Alunos"] = ["Vitor"]
+print (dicionario)
+dicionario = {
+    'Professores' : ['Andre'], ['Denis'],
+    'Alunos' : ['Vitor'],
+}            
